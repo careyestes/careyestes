6ead04e8fb117e59413efe9b53b706442302ad9b
Carey Estes <cestes@sva.edu>
2014-11-03 08:25:38 -0500

    WIP

--- wp-content/themes/basetemplate/admin/enqueueStylesAndScripts.php
+++ wp-content/themes/basetemplate/admin/enqueueStylesAndScripts.php
@@ -6,7 +6,12 @@ function ce_template_scripts_styles() {
 	wp_enqueue_style( 'ce-template-ie', get_template_directory_uri() . '/assets/styles/ie.css', array( 'ce-template-style' ), '20121010' );
 	$wp_styles->add_data( 'ce-template-ie', 'conditional', 'lt IE 9' );
 	wp_enqueue_script( 'js-plugins', get_template_directory_uri().'/assets/js/plugins.min.js', array('jquery') , '20140801', true );
-	wp_enqueue_script( 'js-main', get_template_directory_uri().'/assets/js/main.min.js', array('jquery') , '20140801', true );
+	wp_register_script( 'js-main', get_template_directory_uri().'/assets/js/main.min.js', array('jquery') , '20140801', true );
+	$themeUrl = get_stylesheet_directory_uri();
+	$translation_array = array( 'themeUrl' => $themeUrl);
+	wp_localize_script( 'js-main', 'objectUrl', $translation_array );
+	wp_enqueue_script( 'js-main' );
+	//wp_enqueue_script( 'js-main', get_template_directory_uri().'/assets/js/main.min.js', array('jquery') , '20140801', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'ce_template_scripts_styles' );
--- wp-content/themes/basetemplate/assets/js/build/main.js
+++ wp-content/themes/basetemplate/assets/js/build/main.js
@@ -1,8 +1,38 @@
 jQuery(document).ready(function($) {
+  var url = objectUrl.themeUrl;
 	$( ".supersearch-button" ).click(function() {
 		$('.supersearchOverlay').fadeIn('fast');
 	});
 	$('.searchCloseButton').click(function() {
 		$('.supersearchOverlay').fadeOut('fast');
 	});
+  var countries = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  limit: 10,
+  prefetch: {
+    // url points to a json file that contains an array of country names, see
+    // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
+    url: 'data/search.json',
+    // the json file contains an array of strings, but the Bloodhound
+    // suggestion engine expects JavaScript objects so this converts all of
+    // those strings
+    filter: function(list) {
+      return $.map(list, function(country) { return { name: country }; });
+    }
+  }
+});
+
+// kicks off the loading/processing of `local` and `prefetch`
+countries.initialize();
+
+// passing in `null` for the `options` arguments will result in the default
+// options being used
+$('.supersearch').typeahead(null, {
+  name: 'countries',
+  displayKey: 'title',
+  // `ttAdapter` wraps the suggestion engine in an adapter that
+  // is compatible with the typeahead jQuery plugin
+  source: countries.ttAdapter()
+});
 });
