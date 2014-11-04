c76b40a68a1f7293ccda67a7e02cc4eefabcde9d
Carey Estes <cestes@sva.edu>
2014-11-03 21:14:35 -0500

    WIP

--- wp-content/themes/basetemplate/assets/js/source/main.js
+++ wp-content/themes/basetemplate/assets/js/source/main.js
@@ -6,4 +6,43 @@ jQuery(document).ready(function($) {
 	$('.searchCloseButton').click(function() {
 		$('.supersearchOverlay').fadeOut('fast');
 	});
+
+  var searchTitle = new Bloodhound({
+    datumTokenizer: function (d) { return Bloodhound.tokenizers.whitespace(d.title); },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: {
+      url: 'data/search.json',
+      // ttl: 1
+    }
+  });
+  var searchContent = new Bloodhound({
+    datumTokenizer: function (d) { return Bloodhound.tokenizers.whitespace(d.content); },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: {
+      url: 'data/search.json',
+      // ttl: 1
+      }
+  });
+  searchTitle.initialize();
+  searchContent.initialize();
+
+  var superSearch = $('.supersearch').typeahead({
+    highlight: true
+  },
+  {
+    name: 'superSearchTitle',
+    displayKey: 'title',
+    source: searchTitle.ttAdapter(),
+    templates: {
+      suggestion: Handlebars.compile('<div class="buildingsListItem"><a class="buildingsLinkOut">{{title}}</a></div>')
+    }
+  },
+  {
+    name: 'superSearchContent',
+    displayKey: 'title',
+    source: searchContent.ttAdapter(),
+    templates: {
+      suggestion: Handlebars.compile('<div class="buildingsListItem"><a class="buildingsLinkOut">{{title}}</a></div>')
+    }
+  });
 });
