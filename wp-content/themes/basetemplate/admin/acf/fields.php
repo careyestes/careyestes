359edfdb62dd5c3b90f86e5f47c5d6780b8d6211
Carey Estes <carey.estes@utk.edu>
2014-11-06 17:48:10 -0500

    Added repeater field

--- wp-content/themes/basetemplate/admin/acf/fields.php
+++ wp-content/themes/basetemplate/admin/acf/fields.php
@@ -17,33 +17,60 @@ if(function_exists("register_field_group")) {
 		        'name'          => 'background_image',
 		        'type'          => 'image',
 		        'instructions'  => __('Select the image for a full screen background.'),
-		      ),
+		    ),
+		    array(
+		        'key'           => 'project_background_image_credit',
+		        'label'         => __('Image Credit'),
+		        'name'          => 'background_image_credit',
+		        'type'          => 'text',
+		        'instructions'  => __('Add if this image is not your own.'),
+		    ),
 		    array(
 				'key'          => 'project_custom_color',
 				'label'        => 'Project Color',
 				'name'         => 'custom_color',
 				'type'         => 'color_picker',
 				'instructions' => 'Add custom color for project'
-		      )
 		    ),
-		    'location' => array(
 		    array(
-		        array(
-		          'param'    => 'post_type',
-		          'operator' => '==',
-		          'value'    => 'ce_projects',
-		          'order_no' => 0,
-		          'group_no' => 0,
-		        ),
-		      ),
-		    ),
-		      'options'        => array(
-		        'position'       => 'normal',
-		        'layout'         => 'default',
-		        'hide_on_screen' => array(
-		        ),
-		      ),
-		    'menu_order' => 0,
+        'key' => 'acf_project_anchors',
+        'label' => __('Project Anchors'),
+        'name' => 'project_anchors',
+        'instructions' => 'Add page anchors for the rail',
+        'type' => 'repeater',
+        'sub_fields' => array(
+          array(
+            'key' => 'acf_project_anchor',
+            'label' => 'Anchor',
+            'name' => 'project_anchor',
+            'type' => 'text',
+          ),
+        ),
+        'row_min'      => '',
+        'row_limit'    => '',
+        'layout'       => 'row',
+        'button_label' => 'Add Row',
+      ),
+
+		   ),
+		  'location' => array(
+			  array(
+	        array(
+	          'param'    => 'post_type',
+	          'operator' => '==',
+	          'value'    => 'ce_projects',
+	          'order_no' => 0,
+	          'group_no' => 0,
+	        ),
+	      ),
+	    ),
+	      'options'        => array(
+	        'position'       => 'normal',
+	        'layout'         => 'default',
+	        'hide_on_screen' => array(
+	        ),
+	      ),
+	    'menu_order' => 0,
 	));
 }
 
